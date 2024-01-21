c392f04fe0ace6141bbf26c35e018d37a9df667d
Sophia <s.z.economides@gmail.com>
2024-01-21 14:50:49 +0200

    fix:asset paths

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,7 +8,7 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   getCarouselData() {
-    const carouselDataUrl = `"../../../assets/bat_data.json`;
+    const carouselDataUrl = 'assets/bat_data.json';
     return this.http.get<any>(carouselDataUrl);
   }
 }
