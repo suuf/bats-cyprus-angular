2d678cd960627f9e8da1baa22f81f8c83dd8df62
Sophia <s.z.economides@gmail.com>
2024-04-09 13:33:30 +0300

    feat(translation):add translations for cit-sci component and home page

--- src/app/components/citizen-science-data-widget/citizen-science-data-widget.component.ts
+++ src/app/components/citizen-science-data-widget/citizen-science-data-widget.component.ts
@@ -1,23 +1,35 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CitizenScienceData, DataService } from '../../services/data.service';
 import { CommonModule } from '@angular/common';
+import { TranslationService } from '../../services/translation.service';
+import { SharedModule } from '../../utilities/modules/shared.module';
 
 @Component({
   selector: 'citizen-science-data-widget-component',
   templateUrl: `citizen-science-data-widget.component.html`,
   styleUrl: 'citizen-science-data-widget.component.scss',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   encapsulation: ViewEncapsulation.None,
 })
 export class CitizenScienceDataWidgetComponent implements OnInit {
   citizenScienceData!: CitizenScienceData[];
+  initializedLang: boolean = false;
 
-  constructor(private dataService: DataService) {}
+  constructor(
+    private dataService: DataService,
+    private translationService: TranslationService
+  ) {}
 
   ngOnInit() {
     this.dataService.getObservationData().subscribe((data) => {
       this.citizenScienceData = data.results;
     });
+
+    this.translationService.initializedTranslations$.subscribe(
+      (initialized) => {
+        this.initializedLang = initialized;
+      }
+    );
   }
 }
